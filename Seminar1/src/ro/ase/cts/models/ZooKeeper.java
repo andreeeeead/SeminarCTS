68e5bf8144d0a831db851316d3529a172801a732
Andreea <constantindaniela18@stud.ase.ro>
2021-02-27 14:34:29 +0200

    adding classes ZooKeeper and Animal

---
+++ Seminar1/src/ro/ase/cts/models/ZooKeeper.java
@@ -0,0 +1,22 @@
+package ro.ase.cts.models;
+
+public class ZooKeeper {
+    private String name;
+
+    public ZooKeeper(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void feed(Animal animal) {
+        System.out.println("The zoo keeper " + this.name + " feeds the animal " + animal.getName());
+
+    }
+}
